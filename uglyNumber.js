6f2627d09825d5c0a1ef5a341bfb06e38b3f4096
eksant <eksoct@yahoo.com>
2017-11-16 14:02:28 +0700

    Revisi isUgly without parameters

--- uglyNumber.js
+++ uglyNumber.js
@@ -13,17 +13,17 @@ class UglyNumber {
 
   getUglyNo() {
     let countUglyNumber = 1
-
     while(this.input > countUglyNumber) {
       this.result++;
-      if(this.isUgly(this.result) == 1) {
+      if(this.isUgly() == 1) {
         countUglyNumber++;
       }
     }
     return this.result;
   }
 
-  isUgly(numberUgly) {
+  isUgly() {
+    let numberUgly = this.result
     numberUgly = this.maxDivide(numberUgly, 2);
     numberUgly = this.maxDivide(numberUgly, 3);
     numberUgly = this.maxDivide(numberUgly, 5);
@@ -46,16 +46,16 @@ class UglyNumber {
 //driver code
 let UglyNo150 = new UglyNumber(150)
 console.log(UglyNo150.getUglyNo()); //5832
-console.log(UglyNo150.getUglyArray());
+console.log(UglyNo150.getUglyArray()); //:: panjang hasilnya :) ::
 
 let UglyNo7 = new UglyNumber(7)
 console.log(UglyNo7.getUglyNo()); //8
-console.log(UglyNo7.getUglyArray());
+console.log(UglyNo7.getUglyArray()); //[ 1, 2, 3, 4, 5, 6, 8 ]
 
 let UglyNo10 = new UglyNumber(10)
 console.log(UglyNo10.getUglyNo()); //12
-console.log(UglyNo10.getUglyArray());
+console.log(UglyNo10.getUglyArray()); //[ 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 ]
 
 let UglyNo15 = new UglyNumber(15)
 console.log(UglyNo15.getUglyNo()); //24
-console.log(UglyNo15.getUglyArray());
+console.log(UglyNo15.getUglyArray()); //[ 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16, 18, 20, 24 ]
