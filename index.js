eb1de083537b14b9b68fe11a2feefca57dc097f5
harisman <harismannugraha@gmail.com>
2017-11-16 11:14:51 +0700

    release 0 selesai

---
+++ index.js
@@ -0,0 +1,40 @@
+class  uglyNumber {
+  constructor() {
+    this.i = 1;
+    this.resultArr = [];
+  }
+
+  maxDivide(number, divisible){
+    while(number % divisible == 0) {
+      number = number / divisible;
+    }
+    return number;
+  }
+  isUgly(numberUgly){
+    numberUgly = this.maxDivide(numberUgly, 2);
+    numberUgly = this.maxDivide(numberUgly, 3);
+    numberUgly = this.maxDivide(numberUgly, 5);
+    return (numberUgly === 1) ? true : false;
+  }
+  getUglyNo(input){
+    let countUglyNumber = 1; // ugly number count
+
+    while(input > countUglyNumber) {
+      this.i++;
+      if(this.isUgly(this.i) == 1) {
+        this.getUglyArray(this.i)
+        countUglyNumber++;
+      }
+    }
+    return this;
+  }
+  getUglyArray(output){
+    this.resultArr.push(output);
+    return this;
+  }
+}
+
+console.log(new uglyNumber().getUglyNo(7)); //8
+console.log(new uglyNumber().getUglyNo(10)); //12
+console.log(new uglyNumber().getUglyNo(15)); //24
+// console.log(new uglyNumber().getUglyNo(150)); //5832
